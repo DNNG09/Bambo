809864dee36c670307b6b1185130a527f8c0b4f1
DNNG09 <dylan.lintelo@dyllo-entertainment.nl>
2025-07-29 19:31:19 +0200

    Updated polls permission

--- Cogs/polls.py
+++ Cogs/polls.py
@@ -205,7 +205,7 @@ class Poll(commands.Cog):
         )
 
 
-    @has_permissions(disnake.Permissions.administrator)
+    @has_permissions(administrator=True)
     @commands.slash_command(
         name="pollresults", description="Laat de huidige pollresultaten zien (privé)."
     )
@@ -224,7 +224,7 @@ class Poll(commands.Cog):
         )
         await inter.response.send_message(embed=embed, ephemeral=True)
 
-    @has_permissions(disnake.Permissions.administrator)
+    @has_permissions(administrator=True)
     @commands.slash_command(name="pollvoters", description="Bekijk wie op wat heeft gestemd (alleen zichtbaar voor jou).")
     async def poll_voters(self, inter: disnake.ApplicationCommandInteraction):
         view = active_polls.get(inter.guild.id)
